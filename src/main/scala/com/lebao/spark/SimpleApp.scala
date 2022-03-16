185dc1b39ecd6a721999b8523e9716158851a408
Jason Shen <jason.shen@pwc.com>
2022-03-16 16:35:50 +0800

    update

--- src/main/scala/com/lebao/spark/SimpleApp.scala
+++ src/main/scala/com/lebao/spark/SimpleApp.scala
@@ -5,9 +5,9 @@ import org.apache.spark.sql.SparkSession
 
 object SimpleApp {
   def main(args: Array[String]) {
-    val logFile = "file://C:/work/tools/spark/spark-3.1.3-bin-hadoop3.2/README.md" // Should be some file on your system
-//    val spark = SparkSession.builder.appName("Simple Application").master("local").getOrCreate()
-val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
+    val logFile = "file:///C:/work/tools/spark/spark-3.1.3-bin-hadoop3.2/README.md" // Should be some file on your system
+    val spark = SparkSession.builder.appName("Simple Application").master("local").getOrCreate()
+//val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
     val logData = spark.read.textFile(logFile).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
     val numBs = logData.filter(line => line.contains("b")).count()
